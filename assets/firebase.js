8bbe1e3434c2b42bf859e93d06f5da6ea64a8a52
Fateha Hossain Anushka <129819665+anushka06onu@users.noreply.github.com>
2026-04-19 20:45:11 +0600

    Restore Firebase config to fix blank page issue

--- assets/firebase.js
+++ assets/firebase.js
@@ -1,7 +1,15 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js';
 import { getFirestore, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js';
-import { firebaseConfig } from './config.js';
+
+export const firebaseConfig = {
+  apiKey: 'REMOVED_API_KEY',
+  authDomain: 'gcpc-portal.firebaseapp.com',
+  projectId: 'gcpc-portal',
+  messagingSenderId: 'REMOVED_SENDER_ID',
+  appId: '1:REMOVED_SENDER_ID:web:0521e002f35f817935aedb',
+  measurementId: 'REMOVED_MEASUREMENT_ID'
+};
 
 const app = initializeApp(firebaseConfig);
 

f87bbeb6cb3522b646e33ce33ac51addb08da9c5
Fateha Hossain Anushka <129819665+anushka06onu@users.noreply.github.com>
2026-04-19 20:30:29 +0600

    Fix admin route, add production stability config, and secure firebase credentials

--- assets/firebase.js
+++ assets/firebase.js
@@ -1,15 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js';
 import { getFirestore, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js';
-
-export const firebaseConfig = {
-  apiKey: 'REMOVED_API_KEY',
-  authDomain: 'gcpc-portal.firebaseapp.com',
-  projectId: 'gcpc-portal',
-  messagingSenderId: 'REMOVED_SENDER_ID',
-  appId: '1:REMOVED_SENDER_ID:web:0521e002f35f817935aedb',
-  measurementId: 'REMOVED_MEASUREMENT_ID'
-};
+import { firebaseConfig } from './config.js';
 
 const app = initializeApp(firebaseConfig);
 
--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'REMOVED_API_KEY',
-  authDomain: 'gcpc-portal.firebaseapp.com',
-  projectId: 'gcpc-portal',
-  messagingSenderId: 'REMOVED_SENDER_ID',
-  appId: '1:REMOVED_SENDER_ID:web:0521e002f35f817935aedb',
-  measurementId: 'REMOVED_MEASUREMENT_ID'
-};
